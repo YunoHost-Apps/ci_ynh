f8b99b141bb270941071144c13be52c06a63478f
JensDiemer <git@jensdiemer.de>
2021-01-09 18:16:48 +0100

    redirect to admin if not installs in root domain level

--- conf/urls.py
+++ conf/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 
 # settings.PATH_URL is the $YNH_APP_ARG_PATH
@@ -8,3 +9,6 @@ from django.urls import path
 urlpatterns = [
     path(f'{settings.PATH_URL}/', admin.site.urls),
 ]
+if settings.PATH_URL:
+    # redirect only, if we not installed in root domain level:
+    urlpatterns.append(path('', RedirectView.as_view(pattern_name='admin:index')))
